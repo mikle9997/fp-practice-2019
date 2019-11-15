8c470596d26d7ee675f93553576c6a80c1fa776d
mikle9997 <mikle9997@mail.ru>
2019-11-16 02:14:24 +0300

    add converting func to Task 3.1

--- Task3_1.hs
+++ Task3_1.hs
@@ -6,6 +6,29 @@ module Task3_1 where
   Необходимо реализовать все классы типов, характерные для целых чисел.
 -}
 
-data WeirdPeanoNumber = Zero | Succ WeirdPeanoNumber | Pred WeirdPeanoNumber
+data WeirdPeanoNumber = Zero | Succ WeirdPeanoNumber | Pred WeirdPeanoNumber deriving Show
 
 -- Реализуйте все классы типов, которым должны отвечать целые числа
+
+wpnToInt :: WeirdPeanoNumber -> Int
+wpnToInt Zero = 0
+wpnToInt (Succ x) =   1  + wpnToInt x
+wpnToInt (Pred x) = (-1) + wpnToInt x
+
+intToWPN :: Int -> WeirdPeanoNumber
+intToWPN x
+  | x > 0     = Succ $ intToWPN (x-1)
+  | x < 0     = Pred $ intToWPN (x+1)
+  | otherwise = Zero
+
+-- Ord Enum Show Num Real Real
+-- instance Eq WeirdPeanoNumber where
+
+main :: IO ()
+main = do
+  putStrLn "          -- Suite 1 --"
+  let res1 = wpnToInt $ Pred $ Succ $ Succ $ Succ $ Succ Zero
+  putStrLn $ "wpnToInt $ Pred $ Succ $ Succ $ Succ $ Succ Zero  -> " ++ show res1
+  putStrLn "          -- Suite 2 --"
+  let res2 = intToWPN res1
+  putStrLn $ "intToWPN res1  -> " ++ show res2
