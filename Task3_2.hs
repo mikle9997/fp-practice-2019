17835c60e64f4b953751e35de7c7ed4241473b8b
mikle9997 <mikle9997@mail.ru>
2019-12-07 02:02:04 +0300

    Task 3.2 is ready

--- Task3_2.hs
+++ Task3_2.hs
@@ -35,11 +35,14 @@ instance Ord a => Ord (ReverseList a) where
   (<=) (RCons xs x) (RCons ys y) = x <= y && xs <= ys
 
 instance Monoid (ReverseList a) where
-  mempty  = todo
-  mappend = todo
+  mempty = RNil
+  mappend x    RNil        = x
+  mappend RNil y           = y
+  mappend xs  (RCons ys y) = RCons (xs `mappend` ys) y
 
-instance Functor (ReverseList a) where
-  fmap = todo
+instance Functor ReverseList where
+  fmap _ RNil = RNil
+  fmap f (RCons ys y) = RCons (fmap f ys) (f y)
 
 
 main :: IO ()
@@ -56,3 +59,12 @@ main = do
   putStrLn "\n          -- Suite 4 --"
   let res3 = res1 <= listToRList [1,2,0,3,4,6,7]
   putStrLn $ "res3 : res1 <= listToRList [1,2,0,3,4,6,7]  -> " ++ show res3
+  putStrLn "\n          -- Suite 5 --"
+  let a = listToRList [1,1,2]
+      b = listToRList [3,5]
+      c = listToRList [8,13]
+  let res5 = a `mappend`  mempty `mappend` b  `mappend` c `mappend` mempty
+  putStrLn $ "res5  -> " ++ show res5
+  putStrLn "\n          -- Suite 6 --"
+  let res6 = fmap (+1) (listToRList [1,2,3])
+  putStrLn $ "res6  -> " ++ show res6
