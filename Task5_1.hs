40e99c40aa0e647fbdb661cc557d2e14c4b9284a
mikle9997 <mikle9997@mail.ru>
2020-01-14 23:18:42 +0300

    Task 5.1 is ready

--- Task5_1.hs
+++ Task5_1.hs
@@ -34,10 +34,36 @@ list2dlist' left (h: t) =
 
 -- Реализуйте функции индексирования, вставки и удаления элементов
 index :: DList a -> Int -> a
-index = todo
+index  DNil            _ = error "Index out of range"
+index (DCons _ h _   ) 0 = h
+index (DCons _ h DNil) _ = error "Index out of range"
+index (DCons _ h t   ) i = index t (i-1)
+
 
 insertAt :: DList a -> Int -> a -> DList a
-insertAt list index value = todo
+insertAt  DNil         0 v = DCons DNil v DNil
+insertAt  DNil         _ v = error "Index out of range"
+insertAt (DCons l h r) 0 v = x where
+  x = DCons l v y
+  y = DCons x h r
+insertAt (DCons l h r) i v = DCons l h (insertAt r (i-1) v)
+
 
 removeAt :: DList a -> Int -> DList a
-removeAt list index = todo
+removeAt  DNil            _ = DNil
+removeAt (DCons DNil h r) 0 = r
+removeAt (DCons (DCons ll lh _) _ (DCons _ rh rr)) 0 = x where
+  x = DCons ll lh y
+  y = DCons x  rh rr
+removeAt (DCons l h r)    i = DCons l h (removeAt r (i-1))
+
+
+main :: IO ()
+main = do
+  let dlist = list2dlist [1,2,3,4,5,6,7,8]
+  let res1  = index dlist 4
+  putStrLn $ "index 4          -> " ++ show res1
+  let res2  = insertAt dlist 5 9
+  putStrLn $ "insertAt 5 9     -> " ++ show res2
+  let res3  = removeAt dlist 5
+  putStrLn $ "removeAt dlist 5 -> " ++ show res3
