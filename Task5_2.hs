e34a2f2a65f407493ac3c4c51500d6c639ea60d7
mikle9997 <mikle9997@mail.ru>
2020-01-15 23:44:49 +0300

    Task 5.2 is ready

--- Task5_2.hs
+++ Task5_2.hs
@@ -1,10 +1,11 @@
 module Task5_2 where
 
 import Todo(todo)
+import Prelude hiding (concat)
 
 -- Зиппер из лекции 
 
-data Zipper a = Zipper [a] [a]
+data Zipper a = Zipper [a] [a] deriving (Show)
 
 -- Реализуйте экземпляры классов Show и Eq для этого типа
 
@@ -35,10 +36,25 @@ removeLeft (Zipper (_:lt) r) = Zipper lt r
 -- вставки подсписка в середину и выделения подсписка
 
 concat :: Zipper a -> Zipper a -> Zipper a
-concat left right = todo
+concat (Zipper ll lr) (Zipper rl rr) = Zipper (ll ++ lr) (rl ++ rr)
 
 insertManyAt :: Int -> Zipper a -> Zipper a -> Zipper a
-insertManyAt index what into = todo
+insertManyAt 0    (Zipper ll lr) (Zipper rl rr) = Zipper (rl ++ reverse (ll ++ lr)) rr
+insertManyAt index what           into          = insertManyAt (index - 1) what (goRight into)
 
 subZipper :: Int -> Int -> Zipper a -> Zipper a
-subZipper from to input = todo
+subZipper 0    0  (Zipper l r) = Zipper l []
+subZipper 0    to input        = subZipper 0          (to - 1) (goRight input)
+subZipper from to input        = subZipper (from - 1)  to      (removeRight input)
+
+main :: IO ()
+main = do
+  let zipper_1 = fromList [1,2,3,4]
+  let zipper_2 = fromList [5,6,7,8]
+  let res1  = concat zipper_1 zipper_2
+  putStrLn $ "concat zipper_1 zipper_2         -> " ++ show res1
+  let res2  = insertManyAt 1 zipper_1 zipper_2
+  putStrLn $ "insertManyAt 2 zipper_1 zipper_2 -> " ++ show res2
+  let res31  = subZipper 2 3 zipper_1
+  let res3 = goLeft . goLeft $ res31
+  putStrLn $ "subZipper 2 3 zipper_1           -> " ++ show res3
